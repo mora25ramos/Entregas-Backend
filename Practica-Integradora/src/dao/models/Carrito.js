9148defcfc682cff8c17ba7964cd0372b19356f9
Mora Ramos <mora25ramos@gmail.com>
2023-04-04 20:52:46 -0300

    Subida de cambios en la entrga

--- Practica-Integradora/src/dao/models/Carrito.js
+++ Practica-Integradora/src/dao/models/Carrito.js
@@ -2,9 +2,20 @@ import mongoose from 'mongoose';
 
 const carritoSchema = new mongoose.Schema({
   timestamp: { type: Date, default: Date.now },
-  products: { type: Array, required: true },
+  products:[{ 
+    _id: String, 
+    timestamp: Number, 
+    nombre: String, 
+    descripcion: String, 
+    codigo: String, 
+    foto: String, 
+    precio: Number, 
+    stock: Number 
+  }]
 });
 
 const Carrito = mongoose.model('Carrito', carritoSchema);
 
-export default Carrito
+export default Carrito;
+
+
